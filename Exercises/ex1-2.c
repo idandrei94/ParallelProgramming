e26a5b61a7722842678ae73596ef9314a0ad931c
idandrei94 <idandrei94@gmail.com>
2018-10-19 13:04:58 +0200

    ex 1-2 complete

--- Exercises/ex1-2.c
+++ Exercises/ex1-2.c
@@ -26,10 +26,44 @@ unsigned long long fibonacci_seq(int n)
 	}
 }
 
+unsigned long long fibonacci_list[1000];
+
+unsigned long long fibonacci_rec(int n)
+{
+	switch(n)
+	{
+		case 0: 
+			return 0;
+		case 1:
+			return 1;
+		default:
+		;
+			unsigned long long fib1, fib2;
+			if(fibonacci_list[n-2] == 0) 
+			{
+				fib1 = fibonacci_rec(n-2);
+				fibonacci_list[n-2] = fib1;
+			}
+			else
+				fib1 = fibonacci_list[n-2];
+			if(fibonacci_list[n-1] == 0) 
+			{
+				fib2 = fibonacci_rec(n-1);
+				fibonacci_list[n-1] = fib2;
+			}
+			else
+				fib2 = fibonacci_list[n-1];
+			unsigned long long res = fib1+fib2;
+			fibonacci_list[n] = res;
+			return res;
+			
+	}
+}
+
 int main(void)
 {
 	time_t start = time(NULL);
-	int n = 100;
-	unsigned long long fib = fibonacci_seq(n);
+	int n = 10;
+	unsigned long long fib = fibonacci_rec(n);
 	printf("Completed, Fibonacci #%d is %d, CPU time: %d, Real time: %d\n", n, fib, clock(), start-time(NULL));
 }
